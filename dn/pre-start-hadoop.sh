521a840e6bc759718ec1e0f281e5e1cb87ee42c2
bioshrek <shrekwang1@gmail.com>
2014-10-16 17:36:45 +0000

    add datanode dockerfile

---
+++ dn/pre-start-hadoop.sh
@@ -0,0 +1,13 @@
+# gen core-site.xml
+sed "s/HDFSNAMENODERPC_SERVICE_HOST/$HDFSNAMENODERPC_SERVICE_HOST/;s/HDFSNAMENODERPC_SERVICE_PORT/$HDFSNAMENODERPC_SERVICE_PORT/" /etc/hadoop/conf/core-site.xml.template > /etc/hadoop/conf/core-site.xml
+cat /etc/hadoop/conf/core-site.xml
+
+# log dir
+mkdir -p /home/chianyu/shared_with_docker_container/hadoop-log-dir
+chown -R hdfs:hdfs /home/chianyu/shared_with_docker_container/hadoop-log-dir
+
+# create datanode
+mkdir -p /home/chianyu/shared_with_docker_container/cdh4/dn 
+chown -R hdfs:hdfs /home/chianyu/shared_with_docker_container/cdh4/dn
+chmod 700 /home/chianyu/shared_with_docker_container/cdh4/dn
+
