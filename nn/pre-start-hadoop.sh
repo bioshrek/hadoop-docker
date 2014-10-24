dff4252de787e411285f527edc8a6b9866af3009
bioshrek <shrekwang1@gmail.com>
2014-10-24 03:38:14 +0000

    specify a cluster id to namenode to format

--- nn/pre-start-hadoop.sh
+++ nn/pre-start-hadoop.sh
@@ -7,9 +7,10 @@ mkdir -p /home/chianyu/shared_with_docker_container/hadoop-log-dir
 chown -R hdfs:hdfs /home/chianyu/shared_with_docker_container/hadoop-log-dir
 
 # create and format namenode if not exist
+CLUSTER_ID=CID-c162a031-783e-4db8-823f-ab1ef43dbae8        # an unique cluster id for hadoop cluster
 if [ ! -d /home/chianyu/shared_with_docker_container/cdh5/nn/current ]; then
   mkdir -p /home/chianyu/shared_with_docker_container/cdh5/nn
   chown -R hdfs:hdfs /home/chianyu/shared_with_docker_container/cdh5/nn
   chmod 700 /home/chianyu/shared_with_docker_container/cdh5/nn
-  sudo -u hdfs hdfs namenode -format  # format
+  sudo -u hdfs hdfs namenode -format -clusterId $CLUSTER_ID # always format with the same cluster id
 fi
