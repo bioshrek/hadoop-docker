3d2783f379b9448685ba0d449db68dfacc69b706
bioshrek <shrekwang1@gmail.com>
2014-10-24 02:10:21 +0000

    specify hbase log dir through env

--- hmaster/pre-start-hbase.sh
+++ hmaster/pre-start-hbase.sh
@@ -1,3 +1,8 @@
 # gen hbase-site.xml
 sed "s/HDFSNAMENODERPC_SERVICE_HOST/$HDFSNAMENODERPC_SERVICE_HOST/;s/HDFSNAMENODERPC_SERVICE_PORT/$HDFSNAMENODERPC_SERVICE_PORT/;s/ZOOKEEPERCLIENT_SERVICE_HOST/$ZOOKEEPERCLIENT_SERVICE_HOST/;s/ZOOKEEPERCLIENT_SERVICE_PORT/$ZOOKEEPERCLIENT_SERVICE_PORT/" /etc/hbase/conf/hbase-site.xml.template > /etc/hbase/conf/hbase-site.xml
 cat /etc/hbase/conf/hbase-site.xml
+
+# log dir
+mkdir -p /home/chianyu/shared_with_docker_container/hbase-log-dir
+chown -R hbase:hbase /home/chianyu/shared_with_docker_container/hbase-log-dir
+. /etc/hbase/conf/hbase-env.sh
--- hregionserver/hbase-env.sh
+++ hregionserver/hbase-env.sh
@@ -3,3 +3,6 @@ export JAVA_HOME="/home/chianyu/jdk1.7.0_67"
 
 # tell hbase don't manage zookeeper
 export HBASE_MANAGES_ZK=false
+
+# log dir
+export HBASE_LOG_DIR="/home/chianyu/shared_with_docker_container/hbase-log-dir"
