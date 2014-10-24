d9a49aa34be7ba5df126b685de26c120b2f2e4df
bioshrek <shrekwang1@gmail.com>
2014-10-24 06:18:30 +0000

    config hadoop client for hbase

--- hmaster/pre-start-hbase.sh
+++ hmaster/pre-start-hbase.sh
@@ -2,6 +2,10 @@
 sed "s/HDFSNAMENODERPC_SERVICE_HOST/$HDFSNAMENODERPC_SERVICE_HOST/;s/HDFSNAMENODERPC_SERVICE_PORT/$HDFSNAMENODERPC_SERVICE_PORT/;s/ZOOKEEPERCLIENT_SERVICE_HOST/$ZOOKEEPERCLIENT_SERVICE_HOST/;s/ZOOKEEPERCLIENT_SERVICE_PORT/$ZOOKEEPERCLIENT_SERVICE_PORT/" /etc/hbase/conf/hbase-site.xml.template > /etc/hbase/conf/hbase-site.xml
 cat /etc/hbase/conf/hbase-site.xml
 
+# gen core-site.xml
+sed "s/HDFSNAMENODERPC_SERVICE_HOST/$HDFSNAMENODERPC_SERVICE_HOST/;s/HDFSNAMENODERPC_SERVICE_PORT/$HDFSNAMENODERPC_SERVICE_PORT/" /etc/hadoop/conf/core-site.xml.template > /etc/hadoop/conf/core-site.xml
+cat /etc/hadoop/conf/core-site.xml
+
 # log dir
 mkdir -p /home/chianyu/shared_with_docker_container/hbase-log-dir
 chown -R hbase:hbase /home/chianyu/shared_with_docker_container/hbase-log-dir
