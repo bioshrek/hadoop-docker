345f62090d81d5bbe5163ef97d013beb7d2c1300
bioshrek <shrekwang1@gmail.com>
2014-10-30 07:17:31 +0000

    add zookeeper peer dockerfile

---
+++ zk1/pre-start-zookeeper.sh
@@ -0,0 +1,8 @@
+# log dir
+mkdir -p /home/chianyu/shared_with_docker_container/zookeeper-log-dir
+chown -R zookeeper:zookeeper /home/chianyu/shared_with_docker_container/zookeeper-log-dir
+. /etc/zookeeper/conf/zookeeper-env.sh
+
+# init zookeeper data dir
+chown -R zookeeper:zookeeper /home/chianyu/shared_with_docker_container/zookeeper
+service zookeeper-server init --myid=1
